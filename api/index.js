3ccf432a934befd78c8303f550e28632e201f628
itzhimangshu <itzhimangshu@gmail.com>
2023-11-16 20:56:42 +0530

    create an express api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoute from './routes/user.route.js';
+import reviewRoute from './routes/review.route.js';
+import orderRoute from './routes/order.route.js';
+import messageRoute from './routes/message.route.js';
+import gigRoute from './routes/gig.route.js';
+import conversationRoute from './routes/conversation.route.js';
 dotenv.config();
 
 mongoose
@@ -13,6 +19,13 @@ mongoose
     })
 
 const app = express();
+
+app.use('/api/users', userRoute);
+app.use('/api/reviews', reviewRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/messages', messageRoute);
+app.use('/api/gigs', gigRoute);
+app.use('/api/conversations', conversationRoute);
 
 app.listen(8800, () => {
     console.log('Backend server is running!');
